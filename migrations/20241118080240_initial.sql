77e10b5f37d7882baf76577f7a704a5c4bd35734
wukun <charmfocus@gmail.com>
2024-11-28 11:20:48 +0800

    feature: add triggers for chat/message

--- migrations/20241118080240_initial.sql
+++ migrations/20241118080240_initial.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(64) NOT NULL UNIQUE,
     -- password is stored as a hash argon2
     password_hash VARCHAR(97) NOT NULL,
+    workspace_id BIGINT NOT NULL,
     -- workspace name
     workspace varchar(32) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -15,6 +16,18 @@ CREATE TABLE IF NOT EXISTS users (
 -- create unique index for users for email
 CREATE UNIQUE INDEX IF NOT EXISTS idx_email ON users (email);
 
+-- add index for users for workspace_id
+CREATE INDEX IF NOT EXISTS idx_workspace_id ON users (workspace_id);
+
+-- workspace for users
+CREATE TABLE IF NOT EXISTS workspaces (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL UNIQUE,
+    owner_id BIGINT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_workspace_name ON workspaces (name);
+CREATE INDEX IF NOT EXISTS idx_owner_id ON workspaces (owner_id);
 
 -- create chat type: single group privete_channel public_channel
 CREATE TYPE chat_type AS ENUM ('single', 'group', 'private_channel', 'public_channel');
@@ -22,11 +35,15 @@ CREATE TYPE chat_type AS ENUM ('single', 'group', 'private_channel', 'public_cha
 -- create chat table
 CREATE TABLE IF NOT EXISTS chats (
     id BIGSERIAL PRIMARY KEY,
+    workspace_id BIGINT NOT NULL,
     name VARCHAR(64),
     type chat_type NOT NULL,
     members BIGINT[] NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- add index for chats for workspace_id
+CREATE INDEX IF NOT EXISTS idx_workspace_id ON chats (workspace_id);
 
 -- create message table
 CREATE TABLE IF NOT EXISTS messages (
